8c720412ce5e77e983984bbfb016ca6a9fe990c6
Marcin Trofimiuk <marcin.trofimiuk@gmail.com>
2020-05-20 12:42:58 +0200

    MCS-1788 Do not allow negative values for container delivery, deposit fees and volume

--- containers/models.py
+++ containers/models.py
@@ -1,3 +1,4 @@
+from django.core import validators
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 
@@ -8,12 +9,34 @@ from core.storage.utils import public_image_upload_to
 class Container(BaseAbstractModel):
     size_class = models.CharField(unique=True, max_length=255)
     standard = models.BooleanField(null=True, blank=True)
-    volume = models.FloatField()
+    volume = models.FloatField(
+        validators=[
+            validators.MinValueValidator(
+                0, message=_("Container volume cannot be negative")
+            )
+        ],
+    )
     deposit = models.DecimalField(
-        max_digits=10, decimal_places=2, null=True, blank=True
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        validators=[
+            validators.MinValueValidator(
+                0, message=_("Container deposit cannot be negative")
+            )
+        ],
     )
     delivery_fee = models.DecimalField(
-        max_digits=10, decimal_places=2, null=True, blank=True
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        validators=[
+            validators.MinValueValidator(
+                0, message=_("Container delivery fee cannot be negative")
+            )
+        ],
     )
     image_url = models.CharField(max_length=255, null=True, blank=True)
     image = models.ImageField(upload_to=public_image_upload_to, null=True, blank=True)
