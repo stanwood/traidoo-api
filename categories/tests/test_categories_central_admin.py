afd217f274257034117d4af13897be2f54b909f2
Marcin Trofimiuk <marcin.trofimiuk@gmail.com>
2020-05-20 18:06:36 +0200

    MCS-1787 Allow central admin editing categories

---
+++ categories/tests/test_categories_central_admin.py
@@ -0,0 +1,21 @@
+from django.urls import reverse
+from model_mommy import mommy
+
+
+def test_allow_change_container(central_platform_user, django_app):
+    container = mommy.make_recipe("categories.category")
+
+    response = django_app.get(
+        reverse("admin:categories_category_change", kwargs={"object_id": container.id}),
+        user=central_platform_user,
+    )
+    assert len(response.form.fields) > 1
+
+
+def test_allow_delete_container(central_platform_user, django_app):
+    container = mommy.make_recipe("containers.container")
+    django_app.get(
+        reverse("admin:categories_category_delete", kwargs={"object_id": container.id}),
+        user=central_platform_user,
+        status=302,
+    )
