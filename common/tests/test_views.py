1671f3d063469f2327a4c21e7d3dc016613076c9
Marcin Trofimiuk <marcin.trofimiuk@gmail.com>
2020-05-27 10:23:37 +0200

    MCS-1859 Fix regions test which failed randomly

--- common/tests/test_views.py
+++ common/tests/test_views.py
@@ -8,14 +8,14 @@ def test_get_regions(client_buyer, neighbour_region):
     mommy.make("common.region", id=101, name="Test Region 2")
     mommy.make("common.region", id=102, name="Test Region 3")
 
-    response = client_buyer.get("/regions")
+    response = client_buyer.get("/regions?ordering=name")
 
     assert response.status_code == 200
 
     parsed_response = response.json()
 
     assert parsed_response["results"] == [
-        {"id": 1, "name": 'mcswiss', "slug": ''},
+        {"id": 1, "name": "mcswiss", "slug": ""},
         {"id": neighbour_region.id, "name": "neighbour", "slug": "neighbour"},
         {"id": 100, "name": "Test Region 1", "slug": "test-region-1"},
         {"id": 101, "name": "Test Region 2", "slug": "test-region-2"},
