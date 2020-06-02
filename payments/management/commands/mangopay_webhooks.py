c2db4d903259f5041cde8c67e50542ce6ed2ef31
Marcin Trofimiuk <marcin.trofimiuk@gmail.com>
2020-06-02 15:47:59 +0200

    MCS-1859 Improvement of management command which adds or updates hooks handler url

--- payments/management/commands/mangopay_webhooks.py
+++ payments/management/commands/mangopay_webhooks.py
@@ -6,22 +6,34 @@ from payments.mixins import MangopayMixin
 
 
 class Command(MangopayMixin, BaseCommand):
+    EVENT_TYPES = [
+        "PAYIN_NORMAL_CREATED",
+        "PAYIN_NORMAL_SUCCEEDED",
+        "PAYIN_NORMAL_FAILED",
+        "PAYOUT_NORMAL_SUCCEEDED",
+        "PAYOUT_NORMAL_FAILED",
+        "KYC_SUCCEEDED",
+        "KYC_FAILED",
+    ]
 
     help = "Sets mangopay webhooks handlers"
 
     def add_arguments(self, parser):
-        parser.add_argument('handler_url', type=str)
+        parser.add_argument("handler_url", type=str)
 
     def handle(self, *args, **options):
-        new_hook = options['handler_url']
-        hooks = self.mangopay.get('/hooks')
-        for hook in hooks:
-            self.stdout.write(json.dumps(hook))
-            result = self.mangopay.put(
-                '/hooks/{}'.format(hook['Id']),
-                params={
-                    'Url': new_hook
-                },
-            )
-            self.stdout.write(json.dumps(result))
-
+        new_hook = options["handler_url"]
+        hooks = self.mangopay.get("/hooks")
+        if not hooks:
+            for event_type in self.EVENT_TYPES:
+                hook = self.mangopay.post(
+                    "/hooks/", {"eventType": event_type, "url": new_hook}
+                )
+                self.stdout.write(json.dumps(hook))
+        else:
+            for hook in hooks:
+                self.stdout.write(json.dumps(hook))
+                result = self.mangopay.put(
+                    "/hooks/{}".format(hook["Id"]), {"Url": new_hook},
+                )
+                self.stdout.write(json.dumps(result))
