83c8cba9e4e6f110acf577c383a81aa2590fe275
Marcin Trofimiuk <marcin.trofimiuk@gmail.com>
2020-06-16 11:30:12 +0200

    MCS-1999 Fix has_products parameter parsing in categories endpoint

--- checkout/tests/test_get_checkout_summary.py
+++ checkout/tests/test_get_checkout_summary.py
@@ -65,16 +65,19 @@ def test_get_delivery_options(client_buyer, cart):
     assert cart_item_1.delivery_fee_net == 9.5
     assert cart_item_1._delivery_fee().netto == cart_item_1.delivery_fee_net
 
-    assert items[0]["deliveryOptions"] == [
-        {"id": DeliveryOption.SELLER, "value": 1.1},
-        {"id": DeliveryOption.BUYER, "value": 0.0},
-        {"id": DeliveryOption.CENTRAL_LOGISTICS, "value": cart_item_0.delivery_fee_net},
-    ]
-    assert items[1]["deliveryOptions"] == [
-        {"id": DeliveryOption.SELLER, "value": 1.1},
-        {"id": DeliveryOption.BUYER, "value": 0.0},
-        {"id": DeliveryOption.CENTRAL_LOGISTICS, "value": cart_item_1.delivery_fee_net},
-    ]
+    assert {"id": DeliveryOption.SELLER, "value": 1.1} in items[0]["deliveryOptions"]
+    assert {"id": DeliveryOption.BUYER, "value": 0.0} in items[0]["deliveryOptions"]
+    assert {
+        "id": DeliveryOption.CENTRAL_LOGISTICS,
+        "value": cart_item_0.delivery_fee_net,
+    } in items[0]["deliveryOptions"]
+
+    assert {"id": DeliveryOption.SELLER, "value": 1.1} in items[1]["deliveryOptions"]
+    assert {"id": DeliveryOption.BUYER, "value": 0.0} in items[1]["deliveryOptions"]
+    assert {
+        "id": DeliveryOption.CENTRAL_LOGISTICS,
+        "value": cart_item_1.delivery_fee_net,
+    } in items[1]["deliveryOptions"]
 
     assert (
         parsed_response["deliveryFeeGross"]
