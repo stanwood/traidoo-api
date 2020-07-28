ba24bb946d5829cffc1b4193f29a0ae2c7e24da9
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-07-28 16:16:19 +0200

    [MCS-2070] use partitioning to calculate cart item quantity

--- carts/tests/test_get_cart.py
+++ carts/tests/test_get_cart.py
@@ -6,7 +6,7 @@ from model_bakery import baker
 
 
 @pytest.mark.django_db
-def test_get_cart(buyer, client_buyer, traidoo_region):
+def test_get_cart(buyer, client_buyer):
     delivery_address = baker.make_recipe(
         "delivery_addresses.delivery_address", user=buyer
     )
@@ -21,6 +21,12 @@ def test_get_cart(buyer, client_buyer, traidoo_region):
         product=product_item_1.product,
         quantity=4,
         latest_delivery_date=timezone.now().date() + datetime.timedelta(days=2),
+    )
+
+    product_item_3 = baker.make(
+        "items.item",
+        quantity=10,
+        latest_delivery_date=timezone.now().date() + datetime.timedelta(days=3),
     )
 
     cart = baker.make_recipe(
@@ -41,6 +47,13 @@ def test_get_cart(buyer, client_buyer, traidoo_region):
         product=product_item_2.product,
         latest_delivery_date=product_item_2.latest_delivery_date,
     )
+    cart_item_3 = baker.make_recipe(
+        "carts.cartitem",
+        cart=cart,
+        quantity=4,
+        product=product_item_3.product,
+        latest_delivery_date=product_item_3.latest_delivery_date,
+    )
 
     response = client_buyer.get("/cart")
 
@@ -53,6 +66,14 @@ def test_get_cart(buyer, client_buyer, traidoo_region):
                 "price": float(product_item_1.product.price),
                 "unit": product_item_1.product.unit,
                 "quantity": cart_item_1.quantity + cart_item_2.quantity,
-            }
+            },
+            {
+                "id": product_item_3.product.id,
+                "amount": float(product_item_3.product.amount),
+                "name": product_item_3.product.name,
+                "price": float(product_item_3.product.price),
+                "unit": product_item_3.product.unit,
+                "quantity": cart_item_3.quantity,
+            },
         ],
     }
