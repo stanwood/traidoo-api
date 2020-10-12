31cb0d6a1bf4f64785f9b40ff944dd396015c7bc
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-10-12 06:19:31 +0200

    [MCS-2139] missing apostrophe

--- .build/tests.sh
+++ .build/tests.sh
@@ -3,5 +3,5 @@ set -e
 
 pip install -r requirements.txt
 pip install -r requirements-tests.txt
-sed -i s/POSTGRESQL_HOST=localhost/POSTGRESQL_HOST=postgres/g' .env.test
+sed -i 's/POSTGRESQL_HOST=localhost/POSTGRESQL_HOST=postgres/g' .env.test
 pytest
