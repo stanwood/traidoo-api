b0e76d4bc8e2e79c399271251ab266744966b512
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-10-19 14:58:36 +0200

    [MCS-2153] custom error 400 middleware/logger

--- Traidoo/settings/django/middleware.py
+++ Traidoo/settings/django/middleware.py
@@ -8,6 +8,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "request_logging.middleware.LoggingMiddleware",
     "core.middleware.region.region_middleware",
+    "core.middleware.validation_errors.validation_errors_middleware",
 ]
