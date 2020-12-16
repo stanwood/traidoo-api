62f6d4b4a5b8443618878d55cd54fa64817e8ec9
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-12-16 07:59:22 +0100

    [MCS-2239] Disable mail admins logger

--- Traidoo/settings/django/logging.py
+++ Traidoo/settings/django/logging.py
@@ -3,10 +3,6 @@ LOGGING = {
     "disable_existing_loggers": False,
     "handlers": {
         "console": {"class": "logging.StreamHandler"},
-        "mail_admins": {
-            "level": "ERROR",
-            "class": "django.utils.log.AdminEmailHandler",
-        },
     },
     "loggers": {
         "app": {"handlers": ["console"], "level": "DEBUG", "propagate": False},
