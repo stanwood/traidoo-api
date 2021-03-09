9c86ce952b5e2e15a93bbb2a95d230fb7e9a77dd
Marcin Trofimiuk <marcin.trofimiuk@gmail.com>
2021-03-09 14:17:31 +0100

    MCS-2276 set non cooperative fee from environment variable

--- Traidoo/settings/project/base.py
+++ Traidoo/settings/project/base.py
@@ -1,7 +1,12 @@
 from decimal import Decimal
 
+import environ
+
+env = environ.Env()
 
 CART_LIFESPAN = 60
 UNVERIFIED_USER_LIFE_HOURS = 72
 EARLIEST_DELIVERY_DATE_DAYS_RANGE = (1, 14)
-NON_COOPERATIVE_MEMBERS_PLATFORM_FEE = Decimal("2.0")
+NON_COOPERATIVE_MEMBERS_PLATFORM_FEE = Decimal(
+    env("NON_COOPERATIVE_MEMBERS_PLATFORM_FEE", default="2.0", cast=str)
+)
